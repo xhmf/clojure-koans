35883d470ee513142e65a931b472a275a42b8c5c
ly <lawrence.ying@utexas.edu>
2015-05-08 22:53:55 -0500

    koans 1-16

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -3,21 +3,27 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (nil? (peek coll))
+    coll
+  (cons (peek coll) (recursive-reverse (pop coll)))))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         acc 1]
+    (if (= n 0)
+      acc
+      (recur (dec n) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
@@ -51,4 +57,5 @@
   (< 1000000000000000000000000N (factorial 1000N))
 
   "But what happens when the machine limits you?"
-  (< 1000000000000000000000000N (factorial 100003N)))
+  ;;(< 1000000000000000000000000N (factorial 100003N)))
+  )
